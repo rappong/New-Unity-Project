fa56fbcbf79cf7f56cb315e1e5da4133720cdacf
rappong <55365884+rappong@users.noreply.github.com>
2021-03-16 11:56:35 +0900

    Initial commit

---
+++ Assets/RouletteController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouletteController : MonoBehaviour
+{
+    float rotspeed = 0;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.rotspeed = 5.0f;
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            while (this.rotspeed >= 1)
+            {
+                this.rotspeed = this.rotspeed * 0.0009f;
+            }
+        }
+        transform.Rotate(0, 0, this.rotspeed);
+    }
+}
